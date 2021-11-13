339170f16b928cd8f5062f997df42f801498156c
Anikkhan-web <anikkhan9501@gmail.com>
2021-11-13 22:34:13 +0600

    ok

--- index.js
+++ index.js
@@ -135,15 +135,7 @@ async function run()
 
 
 
-        // delete api
-        // app.delete('/services/:id', async (req, res) =>
-        // {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await servicesCollection.deleteOne(query);
-        //     res.json(result);
 
-        // })
 
 
         // admin and general user
